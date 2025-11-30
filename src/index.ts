6a38ab997b848b29ce3eafd8d093c66b12160749
SaulP4rdo <spiderboy_pardo@hotmail.com>
2025-11-30 03:18:15 -0600

    america

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
-const app = new Hono();
+const app = new Hono()
 
-await delay(5000);
+await delay(5000)
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
-});
+app.get('/', (c) => {
+  return c.text('Hola putillos, eliminaron al america')
+})
 
-app.get("/health", (c) => {
-	return c.json("ok", 200);
-});
+app.get('/health', (c) => {
+  return c.json('ok', 200)
+})
 
-const port = 3000;
-console.log(`Server is running on http://localhost:${port}`);
+const port = 3000
+console.log(`Server is running on http://localhost:${port}`)
 
 serve({
-	fetch: app.fetch,
-	port,
-});
+  fetch: app.fetch,
+  port,
+})
